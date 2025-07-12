adc4b79cd3776f2f9ef4ee0eaee2326c70a362e2
bin3ziz <jok7xx@gmail.com>
2025-07-12 04:40:44 +0300

    Add files via upload

---
+++ gx5/HardwareInfo.c
@@ -0,0 +1,72 @@
+#ifndef _IRDISTANCE_
+#define _IRDISTANCE_ unsigned long
+#endif
+#ifndef _MOTOR_
+#define _MOTOR_ unsigned long
+#endif
+#ifndef _PHOTOSENSOR_
+#define _PHOTOSENSOR_ unsigned long
+#endif
+#ifndef _COMPASS_
+#define _COMPASS_ unsigned long
+#endif
+#ifndef _COMPOUNDEYE3_
+#define _COMPOUNDEYE3_ unsigned long
+#endif
+#ifndef _IRDISTANCE_ur_
+#define _IRDISTANCE_ur_ 672
+#endif
+#ifndef _IRDISTANCE_ul_
+#define _IRDISTANCE_ul_ 288
+#endif
+#ifndef _IRDISTANCE_ub_
+#define _IRDISTANCE_ub_ 16
+#endif
+#ifndef _MOTOR_m1_
+#define _MOTOR_m1_ 124176
+#endif
+#ifndef _MOTOR_m2_
+#define _MOTOR_m2_ 241040
+#endif
+#ifndef _MOTOR_m3_
+#define _MOTOR_m3_ 206528
+#endif
+#ifndef _MOTOR_m4_
+#define _MOTOR_m4_ 140480
+#endif
+#ifndef _PHOTOSENSOR_fp_
+#define _PHOTOSENSOR_fp_ 896
+#endif
+#ifndef _PHOTOSENSOR_bp_
+#define _PHOTOSENSOR_bp_ 512
+#endif
+#ifndef _PHOTOSENSOR_rp_
+#define _PHOTOSENSOR_rp_ 640
+#endif
+#ifndef _PHOTOSENSOR_lp_
+#define _PHOTOSENSOR_lp_ 1232
+#endif
+#ifndef _PHOTOSENSOR_bbp_
+#define _PHOTOSENSOR_bbp_ 160
+#endif
+#ifndef _PHOTOSENSOR_llp_
+#define _PHOTOSENSOR_llp_ 288
+#endif
+#ifndef _PHOTOSENSOR_rrp_
+#define _PHOTOSENSOR_rrp_ 672
+#endif
+#ifndef _COMPASS_compass_
+#define _COMPASS_compass_ 26880
+#endif
+#ifndef _COMPOUNDEYE3_front_
+#define _COMPOUNDEYE3_front_ 90240
+#endif
+#ifndef _COMPOUNDEYE3_back_
+#define _COMPOUNDEYE3_back_ 235664
+#endif
+#ifndef _TASK_1_
+#define _TASK_1_ 1
+#endif
+#ifndef _TASK_2_
+#define _TASK_2_ 1
+#endif
